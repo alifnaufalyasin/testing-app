f8489fced636e674bc0e3e45971bba5ffabbc119
alifnaufalyasin <alifnaufal171199@gmail.com>
2020-05-11 08:02:22 +0700

    edit

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const { handleMessage } = require("./handler/message");
-const { dbinit } = require("./module/db");
 
 module.exports = async function App(context) {
-  dbinit()
-  
   if (context.event.isPosback){
   }else{
     return handleMessage(context);
